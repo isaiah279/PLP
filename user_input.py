1344a9fe32975914c1fb05a47c5237b5c8086605
Isayah Okech <81564990+isaiah279@users.noreply.github.com>
2024-09-06 00:56:43 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,5 @@
+name=input("enter your name:")
+age=input("Enter your age:")
+location=input("enter your location")
+print(f"Hello!{name} i can see you are {age} years Old, and Live "
+      f"in {location}")
